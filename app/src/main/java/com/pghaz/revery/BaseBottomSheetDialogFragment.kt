f3f7ef85a6b9b11d39824ca3d4e1b3c5202186ca
Pierre Ghazal <pg.ghazal@gmail.com>
2020-10-09 19:55:28 +0200

    Expand BottomSheetDialog by default

--- app/src/main/java/com/pghaz/revery/BaseBottomSheetDialogFragment.kt
+++ app/src/main/java/com/pghaz/revery/BaseBottomSheetDialogFragment.kt
@@ -1,11 +1,16 @@
 package com.pghaz.revery
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
 
 abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
@@ -24,5 +29,23 @@ abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         configureViews(savedInstanceState)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+
+        bottomSheetDialog.setOnShowListener { dialog ->
+            configureBottomSheetDialog(dialog as BottomSheetDialog)
+        }
+
+        return bottomSheetDialog
+    }
+
+    private fun configureBottomSheetDialog(bottomSheetDialog: BottomSheetDialog) {
+        val bottomSheet =
+            bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.isDraggable = false
     }
 }
