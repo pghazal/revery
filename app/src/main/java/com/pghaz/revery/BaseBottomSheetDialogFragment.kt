6fc13a66ca324b318d99fc71a484cb949ec78be0
Pierre Ghazal <pg.ghazal@gmail.com>
2020-10-09 20:30:36 +0200

    Make bottom sheet dialog top corners rounded

--- app/src/main/java/com/pghaz/revery/BaseBottomSheetDialogFragment.kt
+++ app/src/main/java/com/pghaz/revery/BaseBottomSheetDialogFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
+import androidx.core.view.ViewCompat
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 
 
 abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
@@ -17,6 +20,10 @@ abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
     @LayoutRes
     abstract fun getLayoutResId(): Int
     abstract fun configureViews(savedInstanceState: Bundle?)
+
+    override fun getTheme(): Int {
+        return R.style.ReveryBottomSheetDialog
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,7 +52,44 @@ abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
         val bottomSheet =
             bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
         val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        // Make dialog top corners rounded when dialog is expanded
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    //In the EXPANDED STATE apply a new MaterialShapeDrawable with rounded corners
+                    val newMaterialShapeDrawable = createMaterialShapeDrawable(bottomSheet)
+                    ViewCompat.setBackground(bottomSheet, newMaterialShapeDrawable)
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // do nothing
+            }
+        })
+
+        // This must be called after ´addBottomSheetCallback()´
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         bottomSheetBehavior.isDraggable = false
+    }
+
+    private fun createMaterialShapeDrawable(bottomSheet: View): MaterialShapeDrawable {
+        val shapeAppearanceModel =
+            //Create a ShapeAppearanceModel with the same shapeAppearanceOverlay used in the style
+            ShapeAppearanceModel.builder(context, 0, R.style.ReveryShapeAppearanceBottomSheetDialog)
+                .build()
+
+        //Create a new MaterialShapeDrawable (you can't use the original MaterialShapeDrawable in the BottoSheet)
+        val currentMaterialShapeDrawable = bottomSheet.background as MaterialShapeDrawable
+        val newMaterialShapeDrawable = MaterialShapeDrawable(shapeAppearanceModel)
+        //Copy the attributes in the new MaterialShapeDrawable
+        newMaterialShapeDrawable.initializeElevationOverlay(context)
+        newMaterialShapeDrawable.fillColor = currentMaterialShapeDrawable.fillColor
+        newMaterialShapeDrawable.tintList = currentMaterialShapeDrawable.tintList
+        newMaterialShapeDrawable.elevation = currentMaterialShapeDrawable.elevation
+        newMaterialShapeDrawable.strokeWidth = currentMaterialShapeDrawable.strokeWidth
+        newMaterialShapeDrawable.strokeColor = currentMaterialShapeDrawable.strokeColor
+        return newMaterialShapeDrawable
     }
 }
