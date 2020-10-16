f80947d83085d683ae3704164f1cc257a914de0e
Pierre Ghazal <pg.ghazal@gmail.com>
2020-10-16 22:26:55 +0200

    Customize ClockView so that AM/PM shows lower than actual time in size

--- app/src/main/java/com/pghaz/revery/view/ClockView.kt
+++ app/src/main/java/com/pghaz/revery/view/ClockView.kt
@@ -5,12 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Handler
+import android.text.format.DateUtils
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.pghaz.revery.R
-import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -20,16 +21,20 @@ class ClockView @JvmOverloads constructor(
     private var mAttached = false
     private val mHandler = Handler()
 
-    private val timeFormatter = SimpleDateFormat.getTimeInstance(DateFormat.SHORT)
-    private val dateFormatter = SimpleDateFormat.getDateInstance(DateFormat.FULL)
     private var mCalendar: Calendar = Calendar.getInstance()
+    private val time24HourFormatter = SimpleDateFormat("HH:mm", Locale.getDefault())
+    private val time12HourFormatter = SimpleDateFormat("hh:mm", Locale.getDefault())
+    private val timeAmPmFormatter = SimpleDateFormat("a", Locale.getDefault())
+    private val is24HourFormat = android.text.format.DateFormat.is24HourFormat(context)
 
     private val timeTextView: TextView
+    private val amPmTextView: TextView
     private val dateTextView: TextView
 
     init {
         val view = LayoutInflater.from(context).inflate(R.layout.view_clock, this)
         timeTextView = view.findViewById(R.id.timeTextView)
+        amPmTextView = view.findViewById(R.id.amPmTextView)
         dateTextView = view.findViewById(R.id.dateTextView)
     }
 
@@ -77,8 +82,22 @@ class ClockView @JvmOverloads constructor(
     }
 
     private fun updateView() {
-        timeTextView.text = timeFormatter.format(mCalendar.time)
-        dateTextView.text = dateFormatter.format(mCalendar.time)
+        val date = mCalendar.time
+
+        if (is24HourFormat) {
+            amPmTextView.visibility = View.GONE
+            timeTextView.text = time24HourFormatter.format(date)
+        } else {
+            amPmTextView.visibility = View.VISIBLE
+            amPmTextView.text = timeAmPmFormatter.format(date)
+            timeTextView.text = time12HourFormatter.format(date)
+        }
+
+        dateTextView.text = DateUtils.formatDateTime(
+            context,
+            date.time,
+            DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_WEEKDAY
+        )
     }
 
     private fun onTimeChanged() {
