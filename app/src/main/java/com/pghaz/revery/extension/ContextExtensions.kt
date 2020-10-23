deb53f12a86e1a2380a3e4eac6f5125ac1050058
Pierre Ghazal <pg.ghazal@gmail.com>
2020-10-23 03:05:42 +0200

    Add Log. method to ContextExtensions.kt

--- app/src/main/java/com/pghaz/revery/extension/ContextExtensions.kt
+++ app/src/main/java/com/pghaz/revery/extension/ContextExtensions.kt
@@ -25,3 +25,51 @@ fun Context.logError(text: String?, throwable: Throwable?) {
         Log.e(this.javaClass.simpleName, text, throwable)
     }
 }
+
+fun Context.logInfo(text: String) {
+    if (BuildConfig.DEBUG) {
+        Log.i(this.javaClass.simpleName, text)
+    }
+}
+
+fun Context.logInfo(text: String?, throwable: Throwable?) {
+    if (BuildConfig.DEBUG) {
+        Log.i(this.javaClass.simpleName, text, throwable)
+    }
+}
+
+fun Context.logDebug(text: String) {
+    if (BuildConfig.DEBUG) {
+        Log.d(this.javaClass.simpleName, text)
+    }
+}
+
+fun Context.logDebug(text: String?, throwable: Throwable?) {
+    if (BuildConfig.DEBUG) {
+        Log.d(this.javaClass.simpleName, text, throwable)
+    }
+}
+
+fun Context.logWarning(text: String) {
+    if (BuildConfig.DEBUG) {
+        Log.w(this.javaClass.simpleName, text)
+    }
+}
+
+fun Context.logWarning(text: String?, throwable: Throwable?) {
+    if (BuildConfig.DEBUG) {
+        Log.w(this.javaClass.simpleName, text, throwable)
+    }
+}
+
+fun Context.logVerbose(text: String) {
+    if (BuildConfig.DEBUG) {
+        Log.v(this.javaClass.simpleName, text)
+    }
+}
+
+fun Context.logVerbose(text: String?, throwable: Throwable?) {
+    if (BuildConfig.DEBUG) {
+        Log.v(this.javaClass.simpleName, text, throwable)
+    }
+}
