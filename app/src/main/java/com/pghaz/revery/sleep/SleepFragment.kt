a5dfdb653e679d153ceafe93c6b5911f2101a0df
Pierre Ghazal <pg.ghazal@gmail.com>
2020-11-05 14:15:14 +0100

    WIP: SleepFragment.kt

--- app/src/main/java/com/pghaz/revery/sleep/SleepFragment.kt
+++ app/src/main/java/com/pghaz/revery/sleep/SleepFragment.kt
@@ -1,24 +1,21 @@
 package com.pghaz.revery.sleep
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.pghaz.revery.BaseFragment
 import com.pghaz.revery.R
 
-class SleepFragment : Fragment() {
+class SleepFragment : BaseFragment() {
+
+    override fun getLayoutResId(): Int {
+        return R.layout.fragment_sleep
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sleep, container, false)
+    override fun configureViews(savedInstanceState: Bundle?) {
+
     }
 
     companion object {
