e9ac771dab787b175881f88846e684ac7ba29a9f
Pierre Ghazal <pg.ghazal@gmail.com>
2020-11-09 14:40:31 +0100

    Dynamic on boarding title with app name

--- app/src/main/java/com/pghaz/revery/onboarding/OnBoardingIntroFragment.kt
+++ app/src/main/java/com/pghaz/revery/onboarding/OnBoardingIntroFragment.kt
@@ -3,6 +3,7 @@ package com.pghaz.revery.onboarding
 import android.os.Bundle
 import com.pghaz.revery.BaseFragment
 import com.pghaz.revery.R
+import kotlinx.android.synthetic.main.fragment_on_boarding_intro.*
 
 class OnBoardingIntroFragment : BaseFragment() {
 
@@ -11,5 +12,9 @@ class OnBoardingIntroFragment : BaseFragment() {
     }
 
     override fun configureViews(savedInstanceState: Bundle?) {
+        titleTextView.text = String.format(
+            getString(R.string.on_boarding_intro_title),
+            getString(R.string.app_name)
+        )
     }
 }
